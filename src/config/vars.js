d481af2f621839c262d7a291aaf766a4e56c2b43
daksh <dakshpareek7@gmail.com>
2023-11-15 19:27:39 +0530

    Revert "feat: adapt for SSL"
    This reverts commit 2f1fde43b761022c7992cfee21ed967adddddb6f.

--- src/config/vars.js
+++ src/config/vars.js
@@ -4,13 +4,3 @@ require('dotenv-safe').config({
     path: path.join(__dirname, '../../.env'),
     example: path.join(__dirname, '../../.env.example'),
 });
-
-const sslConfig = {
-    key: process.env.SSL_PRIVATE_KEY,
-    cert: process.env.SSL_CERTIFICATE,
-    ca: process.env.SSL_CA, // Optional: If you have a certificate authority (CA) bundle
-};
-
-module.exports = {
-    sslConfig,
-}
